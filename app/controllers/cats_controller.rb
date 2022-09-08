a0f032375203b5d2a07aa3fd7b784f94094dfefe
Dan Cross <dancross96@gmail.com>
2022-09-08 12:48:55 -0500

    addded create action for cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,4 +8,17 @@ class CatsController < ApplicationController
     @cat = Cat.find_by(id: params[:id])
     render 'cats/show'
   end
+
+  def new
+    render 'cats/new'
+  end
+
+  def create
+    cat = Cat.new
+    cat.name = params[:name]
+    cat.age = params[:age]
+    cat.image_url = params[:image_url]
+    cat.save
+    redirect_to "/cats"
+  end
 end
